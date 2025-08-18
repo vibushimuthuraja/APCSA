90c406e810fc342cb7e6d818f8572888c78b47dc
vibushimuthuraja <vibu6915@gmail.com>
2025-08-18 11:35:29 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,78 @@
+class Main {
+  public static void main(String[] args) {
+
+    //Use the // to create single line comments to either add info or to take out code for debugging
+
+    System.out.println("Hello world!");
+
+    //We will be using System.out.println and System.out.print throughout the year. Try using both below to see what the difference is!
+
+
+    System.out.print("hi");
+    System.out.println("hello");
+
+
+    //ANSWER: System.out.println prints the text in quotations then goes to the next line, System.out.print prints the text and stays on the same line
+
+
+
+    //Throughout the year this year we will need to store information. For that we will be using VARIABLES!
+
+    //Java is an Object-Oriented programming language. All variables we use this year will either be OBJECTS or PRIMITIVES
+
+    //There are 8 primitives in Java: int, byte, short, long, float, double, boolean, char
+
+    //For AP we need to know: int, double, boolean
+
+    //List examples of the types below and give definition
+    //int (integer): int is a number like 3
+    //double: double is a decimal with a float(.)
+    //boolean: boolean is a true or false value which means a binary value
+
+    //For now we are just going to work with primitive
+
+    //Create 3 variables of each of the above types (USE GOOD CODING PRACTICE WHEN CREATING THE VARIABLES
+    int myGrade= 3;
+    int myAge= 16;
+    int myBirthday = 4;
+    double myGPA= 4.0;
+    double glucoseLevel= 7.6;
+    double hemoglobinA1C= 6.5;
+    boolean cat= true;
+    boolean dog= false;
+    boolean elephant= true;
+
+
+
+
+
+    //MATH TIME!
+
+    //What are the math operators that we can use?
+    //+ - * / % addition, subraction, multiplication, division, mod
+
+    //Try doing some math operations with numbers. How can we check to see if the math worked?
+    System.out.println(3+4);
+    System.out.println(3*4);
+    System.out.println(4-3);
+    System.out.println(8/4);
+    System.out.println(8%4);
+    //We can check that the math worked by checking in our head 
+
+
+
+
+    //Create codes that will print the following:
+
+    //Odd integers from 1 to 100, inclusive of both
+
+
+
+    //All multiples of 3 from 1 to 100
+
+
+
+
+    //Starting at 1000, print on the same line (with a - [hypthen] between each) all of the numbers that end in 0 going down to 0
+  }
+}
